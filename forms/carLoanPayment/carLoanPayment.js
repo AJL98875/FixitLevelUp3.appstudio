2654e1d3d112bb657e979c786e1b7057543454cc
Anthony Laurenzo <anthonylaurenzo@Anthonys-MacBook-Air-2.local>
2019-09-29 20:24:18 -0500

    Update

--- forms/carLoanPayment/carLoanPayment.js
+++ forms/carLoanPayment/carLoanPayment.js
@@ -1,9 +1,14 @@
 
 function carLoanPayment(principal,interest,periods) {
-  let answer = ((principal + interest) / periods)
+  let answer = principal*((interest*((1+interest)**(periods)))/(((1+interest)**(periods)-1)))
   return answer
 }
 
-let loanPayment = carLoanPayment(20000,2000,36)
+let principal = prompt('What is the principal?')
+let interest = prompt('What is the interest?')
+let periods = prompt('What are the periods in months?')
+
+let loanPayment = carLoanPayment( principal, interest, periods)
 alert('Your Car loan payment is ' + Math.round(loanPayment) + ' a month') 
+
 
