85da0613b6b716a890f3191d9ae5ebc6fd048c18
Anthony Laurenzo <anthonylaurenzo@Anthonys-MacBook-Air-2.local>
2019-09-29 21:25:17 -0500

    Car Loan Finished

--- forms/carLoanPayment/carLoanPayment.js
+++ forms/carLoanPayment/carLoanPayment.js
@@ -1,14 +1,10 @@
 
-function carLoanPayment(principal,interest,periods) {
-  let answer = principal*((interest*((1+interest)**(periods)))/(((1+interest)**(periods)-1)))
+function carLoanPayment(principalCar,interestCar,periodsCar) {
+  let answer = principalCar * (interestCar*(Math.pow(1+interestCar,periodsCar)) / (Math.pow(1+interestCar,periodsCar) -1 ))
   return answer
 }
 
-let principal = prompt('What is the principal?')
-let interest = prompt('What is the interest?')
-let periods = prompt('What are the periods in months?')
 
-let loanPayment = carLoanPayment( principal, interest, periods)
-alert('Your Car loan payment is ' + Math.round(loanPayment) + ' a month') 
+
 
 
